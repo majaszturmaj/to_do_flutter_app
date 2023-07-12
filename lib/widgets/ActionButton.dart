feb67303b287f32359209f15394e18c85afce85f
Maja Szturmaj <maja.szturmaj@gmail.com>
2023-07-12 13:26:59 +0200

    Add main, pretty button

--- lib/widgets/ActionButton.dart
+++ lib/widgets/ActionButton.dart
@@ -27,60 +27,51 @@ class _ActionButtonState extends State<ActionButton> {
         }
       },
       child: Container(
-        width: 75,
-        height: 75,
-        child: Stack(
-          children: <Widget>[
-            Positioned(
-              bottom: 50,
-              left: MediaQuery.of(context).size.width / 2 - 37.5,
-              child: Container(
-                width: 75,
-                height: 75,
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Color.fromRGBO(163, 154, 196, 0.8999999761581421),
-                      offset: Offset(1, 1),
-                      blurRadius: 3,
-                    ),
-                  ],
-                  gradient: LinearGradient(
-                    begin: Alignment(0.5, 0.5),
-                    end: Alignment(-0.5, 0.5),
-                    colors: [
-                      Color.fromRGBO(45, 110, 126, 1),
-                      Color.fromRGBO(10, 83, 160, 1)
-                    ],
-                  ),
-                  borderRadius: BorderRadius.all(Radius.elliptical(75, 75)),
+          width: 75,
+          height: 75,
+          child: Stack(
+              children: <Widget>[
+                Positioned(
+                    top: 0,
+                    left: 0,
+                    child: Container(
+                        width: 75,
+                        height: 75,
+                        decoration: BoxDecoration(
+                          boxShadow : [BoxShadow(
+                              color: Color.fromRGBO(163, 154, 196, 0.8999999761581421),
+                              offset: Offset(1,1),
+                              blurRadius: 3
+                          )],
+                          gradient : LinearGradient(
+                              begin: Alignment(0.5,0.5),
+                              end: Alignment(-0.5,0.5),
+                              colors: [Color.fromRGBO(45, 110, 126, 1),Color.fromRGBO(10, 83, 160, 1)]
+                          ),
+                          borderRadius : BorderRadius.all(Radius.elliptical(75, 75)),
+                        )
+                    )
+                ),Positioned(
+                    top: 11,
+                    left: 10,
+                    child: Container(
+                        width: 54,
+                        height: 54,
+                        decoration: BoxDecoration(
+                          image : DecorationImage(
+                              image: AssetImage(
+                                isOnNoteEdit ? 'lib/assets/images/save.png' : 'lib/assets/images/plusmath.png',
+                              ),
+                              fit: BoxFit.fitWidth,
+                              colorFilter: ColorFilter.mode(Colors.white.withOpacity(0.8),
+                                                            BlendMode.srcIn,
+                              ),
+                          ),
+                        )
+                    )
                 ),
-              ),
-            ),
-            Positioned(
-              bottom: 39,
-              left: MediaQuery.of(context).size.width / 2 - 27,
-              child: Container(
-                width: 54,
-                height: 54,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(
-                      isOnNoteEdit
-                          ? 'assets/images/Save.png'
-                          : 'assets/images/Plusmath.png',
-                    ),
-                    fit: BoxFit.fitWidth,
-                    colorFilter: ColorFilter.mode(
-                      Colors.white.withOpacity(0.8),
-                      BlendMode.dstATop,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+              ]
+          )
       ),
     );
   }
